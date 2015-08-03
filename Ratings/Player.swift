38ebd02a901098efc6d4137a4f7594c12444ca62
Calvin Cheng <linchuan.cheng@gmail.com>
2015-08-03 15:03:26 +0800

    Gestures

---
+++ Ratings/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  Ratings
+//
+//  Created by Calvin Cheng on 3/8/15.
+//  Copyright © 2015 Hello HQ Pte. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class Player: NSObject {
+
+}
