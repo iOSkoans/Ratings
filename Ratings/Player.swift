cc8f4cf4615bffccd8d967ea6f69d70197e265d1
Calvin Cheng <linchuan.cheng@gmail.com>
2015-08-03 15:03:46 +0800

    Player model

--- Ratings/Player.swift
+++ Ratings/Player.swift
@@ -10,4 +10,14 @@ import UIKit
 
 class Player: NSObject {
 
+    var name: String
+    var game: String
+    var rating: Int
+    
+    init(name: String, game: String, rating: Int) {
+        self.name = name
+        self.game = game
+        self.rating = rating
+        super.init()
+    }
 }
